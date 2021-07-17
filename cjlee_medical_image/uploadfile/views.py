f90b9e76151f81383c4fdcbd50cc1c8756841cd5
Rick Chen <rick20001010@gmail.com>
2021-07-17 18:04:27 +0800

    20210717

--- cjlee_medical_image/uploadfile/views.py
+++ cjlee_medical_image/uploadfile/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from .forms import uploadmodelform
 from .models import photo
+
 # Create your views here.
 def index(request):
     #pic_obj = models.photo.objects.clear()
@@ -17,4 +18,25 @@ def index(request):
         'preview':preview
     }
     return render(request, 'test2/second.html', context)    
+
+def pred_demo(request):
+    return render(request,'test3/unet_pred_demo.html')
+
+def unet_pred(request):
+    return render(request,'test3/unet_pred.html')
+
+
+from from werkzeug.utils import secure_filename
+from keras.applications.mobilenet import preprocess_input as preprocess_1
+from keras.applications.resnet import preprocess_input as preprocess_2
+from keras.applications.densenet import preprocess_input as preprocess_3
+import joblib
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from keras_unet_collection import utils
+import cv2
+from PIL import Image
+import shutil
 
--- justin_lin/justin_lin/urls.py
+++ justin_lin/justin_lin/urls.py
@@ -1,4 +1,4 @@
-"""cjlee_medical_image URL Configuration
+"""justin_lin URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -13,11 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import path
-from . import views
-urlpatterns = [
-    path('',views.index,name='index'),
-    path('pred_demo',views.pred_demo,name='pred_demo'),
-    path('unet_pred',views.unet_pred,name='unet_pred'),
-]
 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
